4fa56eaf175f892d9f9a1cbc15aa64d5345fdadc
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2018-06-17 20:28:42 +0930

    Added recipe item to list component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../recipe.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input()
+  public recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- recipes: Recipe[] = [];
+ recipes: Recipe[] = [
+   // tslint:disable-next-line:max-line-length
+   new Recipe('Roasted Ribs', 'Roasted ribs with sweet chilly and a salad side dish', 'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+   new Recipe('Kabobs', 'Shish kabobs over an open flame', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg')
+ ];
 
    constructor() { }
 
